f6d3697976a8b2e647f51ddeb4c16ab5ab97bd83
Govendyaev Sergey <govendyaev.sergey@gmail.com>
2022-11-23 23:09:56 +0500

    Добавляет новые компоненты

---
+++ project/src/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+import {memo} from 'react';
+import Logo from '../logo/logo';
+
+function Footer(): JSX.Element {
+  return (
+    <footer className="page-footer">
+      <Logo light />
+      <div className="copyright">
+        <p>© 2019 What to watch Ltd.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default memo(Footer);
---
+++ project/src/pages/error-screen/error-screen.tsx
@@ -0,0 +1,23 @@
+import {useAppDispatch} from '../../hooks';
+import {fetchFilmsAction} from '../../store/api-actions';
+
+function ErrorScreen(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  return (
+    <>
+      <p className="error__text">Не удалось загрузить Фильмы</p>
+      <button
+        onClick={() => {
+          dispatch(fetchFilmsAction());
+        }}
+        className="replay replay--error"
+        type="button"
+      >
+        Попробовать ещё раз
+      </button>
+    </>
+  );
+}
+
+export default ErrorScreen;
